59160aa5c5af4153058984f81efd9f9cc726e3e3
marta.adell.noguera <marta.adell.noguera@gmail.com>
2017-10-20 16:05:41 +0200

    Correcció Layout i mostrar llista

--- app/src/main/java/edu/upc/eseiaat/pma/shoppinglist/ShoppingListActivity.java
+++ app/src/main/java/edu/upc/eseiaat/pma/shoppinglist/ShoppingListActivity.java
@@ -1,13 +1,42 @@
 package edu.upc.eseiaat.pma.shoppinglist;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ShoppingListActivity extends AppCompatActivity {
+
+    private ArrayList<String> itemlist;
+    private ArrayAdapter<String> adapter;
+
+    private ListView list;
+    private Button btn_add;
+    private EditText edit_item;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shopping_list);
+
+        list = (ListView) findViewById(R.id.list);
+        btn_add = (Button) findViewById(R.id.btn_ad);
+        edit_item = (EditText) findViewById(R.id.edit_item);
+
+        itemlist = new ArrayList<>();
+        itemlist.add("Patatas");
+        itemlist.add("Papel de vater");
+        itemlist.add("Zanahorias");
+        itemlist.add("Copas danone");
+
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, itemlist);
+
+
+        list.setAdapter(adapter);
+
     }
 }
