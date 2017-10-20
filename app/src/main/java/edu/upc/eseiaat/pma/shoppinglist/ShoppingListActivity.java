b467df1e7a810b37509826c48fa1de1fa3bf1673
marta.adell.noguera <marta.adell.noguera@gmail.com>
2017-10-20 16:08:56 +0200

    Eliminar element de la llista al apretar-lo (falta quadre de diàleg)

--- app/src/main/java/edu/upc/eseiaat/pma/shoppinglist/ShoppingListActivity.java
+++ app/src/main/java/edu/upc/eseiaat/pma/shoppinglist/ShoppingListActivity.java
@@ -2,11 +2,14 @@ package edu.upc.eseiaat.pma.shoppinglist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -43,9 +46,29 @@ public class ShoppingListActivity extends AppCompatActivity {
             }}
         );
 
+        edit_item.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                addItem();
+                return true;
+            }
+        });
+
+        list.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> list, View item, int position, long id) {
+                maybeRemoveitem(position);
+                return true;
+            }
+        });
 
         list.setAdapter(adapter);
 
+    }
+
+    private void maybeRemoveitem(int position) {
+        itemlist.remove(position);
+        adapter.notifyDataSetChanged();
     }
 
     private void addItem() {
