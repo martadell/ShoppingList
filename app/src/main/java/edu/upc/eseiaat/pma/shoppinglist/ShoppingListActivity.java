3ddc0480fe339c7d85f51e36b8ca1a2575a38930
marta.adell.noguera <marta.adell.noguera@gmail.com>
2017-10-20 15:57:58 +0200

    Creació de l'aplicació

---
+++ app/src/main/java/edu/upc/eseiaat/pma/shoppinglist/ShoppingListActivity.java
@@ -0,0 +1,13 @@
+package edu.upc.eseiaat.pma.shoppinglist;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ShoppingListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shopping_list);
+    }
+}
